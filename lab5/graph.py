8cc18303cf7de5056f166e0eb18fcbd015451f58
Vladimir Doborschuk <w.doborschuk@gmail.com>
2024-01-11 09:12:47 +0300

    lab6: finished

--- lab5/graph.py
+++ lab5/graph.py
@@ -1,14 +1,34 @@
 import matplotlib.pyplot as plt
-import pandas
+import numpy as np
+import pandas as pd
 
-if __name__ == "__main__":
-    result = pandas.read_csv('data.csv')
+a = 5
 
-    x = result['x']
-    y = result['y']
-    in_p = result['mask'] == 1
-    out_p = result['mask'] == 0
+# Считываем данные из файла
+data = pd.read_csv('points.csv', sep=',', names=['x', 'y', 'isIn'], header=0)
 
-    plt.scatter(x[in_p], y[in_p], color='red', marker='.')
-    plt.scatter(x[out_p], y[out_p], marker='.')
-    plt.savefig('res.jpg')
+# Разделяем точки на те, что попали в область, и те, что нет
+inside = data[data['isIn'] == True]
+outside = data[data['isIn'] == False]
+
+# Рисуем точки
+plt.scatter(inside['x'], inside['y'], color='green', alpha=0.6, s=1)
+plt.scatter(outside['x'], outside['y'], color='gray', alpha=0.4, s=1)
+
+# Рисуем лемнискату Бернулли
+phi = np.linspace(-np.pi/4, np.pi/4, 1000)
+r = np.sqrt(2*a**2 * np.cos(2*phi))
+x = r * np.cos(phi)
+all_x = np.concatenate((x, -x))
+y = r * np.sin(phi)
+all_y = np.concatenate((y, -y))
+plt.plot(all_x, all_y, color='red')
+
+# Рисуем окружность радиуса a
+phi = np.linspace(-np.pi, np.pi, 1000)
+circle_x = a * np.cos(phi)
+circle_y = a * np.sin(phi)
+plt.plot(circle_x, circle_y, color='black')
+
+#plt.legend()
+plt.savefig('area.jpg', dpi=600)
--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,97 +1,83 @@
+#include <cmath>
 #include <vector>
 #include <thread>
+#include <mutex>
 #include <random>
+#include <chrono>
 #include <iostream>
 #include <fstream>
-#include <functional>
-#include <mutex>
 
-using namespace std;
-
-const auto N_PROC = 8;
-const int Total_points = 100000;
 const int a = 5;
-struct point_t{
-    double x, y;
-};
-vector<point_t> in;
-vector<point_t> out;
+const int num_points = 10000;
+const int num_threads = std::thread::hardware_concurrency();
 
-mutex vectorMutex;
+std::mutex vectorMutex;
+std::mutex fileMutex;
+std::vector<int> results(num_threads);
+std::ofstream file("points.csv");
 
-void gen_test_Point(int num_points, const function<bool(double, double)>& area) {
-    int local_in = 0;
-    int local_out = 0;
-
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_real_distribution<> distribX((sqrt(3)/2)*a, a*sqrt(2));
-    uniform_real_distribution<> distribY(-static_cast<double>(a)/2, static_cast<double>(a)/2);
-
-    for (int i = 0; i < num_points; ++i) {
-        point_t p;
-
-        p.x = distribX(gen) * pow(-1, i);
-        p.y = distribY(gen);
-        std::lock_guard<std::mutex> guard(vectorMutex);
-
-        if (area(p.x, p.y)) {
-            in.push_back(p);
-        } else {
-            out.push_back(p);
-        }
-    }
-
+bool isInArea(double x, double y) {
+    double r = sqrt(x*x + y*y);
+    double phi = atan2(y, x);
+    return r*r <= 2*a*a*std::cos(2*phi) && r >= a;
 }
 
-bool func(double i, double j) {
-    if (i < a) {
-        return (i*i+j*j >= a*a) && (pow(i*i+j*j, 2) <= 2*a*a*(i*i-j*j));
-    } else {
-        return (pow(i * i + j * j, 2) <= 2 * a * a * (i * i - j * j));
+void generate(int id, int num_points) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> distribX(a*sqrt(3)/2, a*sqrt(2));
+    std::uniform_real_distribution<> distribY(static_cast<double>(-a)/2, static_cast<double>(a)/2);
+
+    int count = 0;
+    for (int i = 0; i < (int) num_points / 2; ++i) {
+        double x = distribX(gen);
+        double y = distribY(gen);
+        bool isInPositive = isInArea(x, y);
+        if (isInPositive) {
+            count++;
+        }
+
+        bool isInNegative = isInArea(-x, y);
+        if (isInNegative) {
+            count++;
+        }
+
+        std::lock_guard<std::mutex> lock(fileMutex);
+        file << x << "," << y << "," << isInPositive << std::endl;
+        file << -x << "," << y << "," << isInNegative << std::endl;
     }
+
+    std::lock_guard<std::mutex> lock(vectorMutex);
+    results[id] = count;
 }
 
 int main() {
-    int num_points = Total_points / N_PROC;
-    vector<thread> threads;
-    threads.reserve(N_PROC);
+    std::vector<std::thread> threads(num_threads);
 
-    const auto t1 = chrono::steady_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
 
-    // Создание и запуск потоков
-    for (auto n_th = 0; n_th < N_PROC; n_th++) {
-        threads.emplace_back(gen_test_Point, num_points, func);
+    file << "x,y,isInArea" << std::endl;
+    for (int i = 0; i < num_threads; ++i) {
+        threads[i] = std::thread(generate, i, (int) num_points / num_threads);
     }
 
-    // Ожидание завершения всех потоков
     for (auto& th : threads) {
         th.join();
     }
 
-
-    const auto t2 = chrono::steady_clock::now();
-
-    ofstream of;
-    of.open("data.csv");
-    of << "x,y,mask" << endl;
-
-    for (const auto &cur: in) {
-        of << cur.x << ',' << cur.y << ',' << true << endl;
+    int total = 0;
+    for (int count : results) {
+        total += count;
     }
 
-    for (const auto &cur: out) {
-        of << cur.x << ',' << cur.y << ',' << false << endl;
-    }
-
-    size_t cnt1 = in.size();
-    size_t cnt2 = out.size();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = end-start;
     double square = ((a*sqrt(2)) - ((sqrt(3)/2)*a)) * (static_cast<double>(a)/2);
 
-    cout << "Total area of data generation   " << square << endl;
-    cout << "Square counted   " << 4*square*(static_cast<double>(cnt1)/(cnt1+cnt2)) << endl;
-    cout << "Theoretical value   " << a*a*((3*sqrt(3) - 3.14159)/3) << endl;
-    cout << chrono::duration_cast<chrono::milliseconds>(t2 - t1).count() << "ms" << endl;
+    double area = static_cast<double>(total) / num_points * (4*square);
+    std::cout << "Calculated area: " << area << std::endl;
+    std::cout << "Theory area: " << (3*sqrt(3) - M_PI) * a * a / 3 << std::endl;
+    std::cout << "Time taken: " << diff.count() << " s\n";
 
     std::system("python3 graph.py");
 
